203817fd7419b6e0058e63c9876f90f84fe7559d
thepercival <coendunnink@gmail.com>
2019-02-25 13:04:42 +0100

    fontawesome update

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,19 +1,21 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { UserRoutingModule } from './user-routing.module';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
 import { LoginComponent } from './login/login.component';
 import { LogoutComponent } from './logout/logout.component';
-import { RegisterComponent } from './register/register.component';
-import { PasswordresetComponent } from './passwordreset/passwordreset.component';
 import { PasswordchangeComponent } from './passwordchange/passwordchange.component';
+import { PasswordresetComponent } from './passwordreset/passwordreset.component';
+import { RegisterComponent } from './register/register.component';
+import { UserRoutingModule } from './user-routing.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
-    UserRoutingModule
+    UserRoutingModule,
+    FontAwesomeModule
   ],
   declarations: [LoginComponent, LogoutComponent, RegisterComponent, PasswordresetComponent, PasswordchangeComponent]
 })
